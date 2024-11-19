f3a400ff1cfc4fde35ea005e63dae040ef25a67f
misash <misash@outlook.com>
2024-11-19 18:43:15 -0500

    process audio and lypsync

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import voice from "elevenlabs-node";
 import express from "express";
 import { promises as fs } from "fs";
 import OpenAI from "openai";
+import fileUpload from 'express-fileupload';
 dotenv.config();
 
 const openai = new OpenAI({
@@ -17,6 +18,7 @@ const voiceID = "Vpv1YgvVd6CHIzOTiTt8";
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload());
 const port = 3000;
 
 app.get("/", (req, res) => {
@@ -37,19 +39,64 @@ const execCommand = (command) => {
 };
 
 const lipSyncMessage = async (message) => {
+  console.log("LYPSYNC MESSAGE!!!")
+  console.log("MESSAGE: ", message)
   const time = new Date().getTime();
-  console.log(`Starting conversion for message ${message}`);
+  console.log(`Starting conversion mp3 to wav for message ${message}`);
   await execCommand(
     `ffmpeg -y -i audios/message_${message}.mp3 audios/message_${message}.wav`
     // -y to overwrite the file
   );
   console.log(`Conversion done in ${new Date().getTime() - time}ms`);
+  console.log("Starting LypSync -> wav to json data")
   await execCommand(
-    `bin\\rhubarb.exe -f json -o audios\\message_${message}.json audios\\message_${message}.wav -r phonetic`
+    `/home/dark/Downloads/rhubarb-bin/Rhubarb-Lip-Sync-1.13.0-Linux/bin/rhubarb -f json -o audios/message_${message}.json audios/message_${message}.wav -r phonetic`
   );
   // -r phonetic is faster but less accurate
   console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
 };
+
+
+const lipSyncAudio = async (audioFilePath) => {
+  const time = new Date().getTime();
+  console.log(`Starting conversion mp3 to wav for ${audioFilePath}`);
+  await execCommand(
+    `ffmpeg -y -i ${audioFilePath} ${audioFilePath.replace('.wav', '.mp3')}`
+  );
+
+  console.log(`Conversion done in ${new Date().getTime() - time}ms`);
+
+  console.log("Starting LipSync -> wav to json data");
+  await execCommand(
+    `/home/dark/Downloads/rhubarb-bin/Rhubarb-Lip-Sync-1.13.0-Linux/bin/rhubarb -f json -o ${audioFilePath.replace('.wav', '.json')} ${audioFilePath} -r phonetic`
+  );
+  console.log(`Lip sync done in ${new Date().getTime() - time}ms`);
+};
+
+
+app.post("/process-audio", async (req, res) => {
+  console.log("PROCESS AUDIO!!!!!!!!!!!!!!!!!!!!!!!!!!")
+  const audioFile = req.files.audio; 
+
+  const audioFilePath = `audios/recorded_audio.wav`;
+  await audioFile.mv(audioFilePath);  
+
+  await lipSyncAudio(audioFilePath);
+
+  const messages = [
+    {
+      text: "Here is your response...",
+      audio: await audioFileToBase64(audioFilePath),
+      lipsync: await readJsonTranscript(audioFilePath.replace('.wav', '.json')),
+      facialExpression: "smile",
+      animation: "Talking_1",
+    },
+  ];
+
+  res.send({ messages });
+});
+
+
 
 app.post("/chat", async (req, res) => {
   const userMessage = req.body.message;
@@ -121,9 +168,13 @@ app.post("/chat", async (req, res) => {
     ],
   });
   let messages = JSON.parse(completion.choices[0].message.content);
+
+  console.log("messages: ", messages)
+
   if (messages.messages) {
     messages = messages.messages; // ChatGPT is not 100% reliable, sometimes it directly returns an array and sometimes a JSON object with a messages property
   }
+
   for (let i = 0; i < messages.length; i++) {
     const message = messages[i];
     // generate audio file
